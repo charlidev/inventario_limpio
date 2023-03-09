77193e47be9c545ddf55639d590652560b932ca5
charlidev <carlos.martinez.cbtis59@gmail.com>
2023-03-09 00:12:26 -0600

    segunda prueba de alertas sin exito

--- dashboard.php
+++ dashboard.php
@@ -9,3 +9,15 @@
         echo "<a href='logout.php'>Salir</a>";
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola carnal</h1>
+</body>
+</html>
